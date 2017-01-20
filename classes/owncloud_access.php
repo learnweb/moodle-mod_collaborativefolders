17a4b3e523eb3f385e2f1843f79e8d4b3322d3f9
walsidalw <kleinfachnull@gmail.com>
2017-01-20 18:55:25 +0100

    Reworked share and link generation.

--- classes/owncloud_access.php
+++ classes/owncloud_access.php
@@ -15,10 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 //
 /**
- * This is a one-line short description of the file
  *
- * You can have a rather longer description of the file as well,
- * if you like, and it can span multiple lines.
  *
  * @package    mod_collaborativefolders
  * @copyright  2016 Westfälische Wilhelms-Universität Münster (WWU Münster)
@@ -28,63 +25,62 @@
 
 namespace mod_collaborativefolders;
 
+global $CFG;
+//require_once($CFG->dirroot.'/lib/setuplib.php');
+
 use moodle_url;
-use tool_oauth2sciebo\sciebo_client;
-use webdav_client;
+use tool_oauth2sciebo\sciebo;
 
 defined('MOODLE_INTERNAL') || die();
-
-global $CFG;
-
-require_once($CFG->dirroot.'/lib/setuplib.php');
 
 class owncloud_access {
 
     /** @var \tool_oauth2sciebo\sciebo client instance for server access. */
     private $sciebo;
 
+    /**
+     * owncloud_access constructor. The OAuth 2.0 client is initialized within it.
+     */
     public function __construct () {
         $returnurl = new moodle_url('/admin/settings.php?section=modsettingcollaborativefolders', [
             'callback'  => 'yes',
             'sesskey'   => sesskey(),
         ]);
-        $this->sciebo = new \tool_oauth2sciebo\sciebo($returnurl);
+        $this->sciebo = new sciebo($returnurl);
     }
 
-    public function add_to_personal_account($url, $scieboidentifier, $id) {
-        // Hardcoded user data here. Has to be replaced as soon as OAuth is ready.
-        // TODO How can requests be send without user data in clear text?
-        $username = 'collaborativefolder.pbox@uni-muenster.de';
-        $password = '';
-        $pref = 'https://';
-
-        $ch = curl_init();
-        // A POST request creating a share for the chosen file is generated here.
-        curl_setopt($ch, CURLOPT_URL, $pref . 'uni-muenster.sciebo.de' . '/ocs/v1.php/apps/files_sharing/api/v1/shares');
-        curl_setopt($ch, CURLOPT_POST, 1);
-        // http_build_query additionally needs a new arg_separator ("&" instead of "&amp;")
-        // to be able to create the message body.
-        // Additional POST arguments can be edited.
-        curl_setopt($ch, CURLOPT_POSTFIELDS,
-            http_build_query(array('path' => $url,
-                'shareType' => 0,
-                'shareWith' => $scieboidentifier,
-                'publicUpload' => true,
-                'permissions' => 4,
-            ), null, "&"));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-        $output = curl_exec($ch);
-        // The output has to be transformed into an xml file to be able to extract specific arguments
-        // of the response from the owncloud Server.
-        $xml = simplexml_load_string($output);
-        curl_close($ch);
-        if ($xml->meta->statuscode == 100 && $xml->meta->status == 'ok') {
-            notice(get_string('successtoaddfolder', 'mod_collaborativefolders'), new moodle_url('/mod/collaborativefolders/view.php', array('id' => $id)));
+    /**
+     * Method for share creation in ownCloud. A share for a specific user and folder is generated.
+     * @param $path string path to the folder.
+     * @param $userid string username in ownCloud.
+     * @return string link to the folder.
+     */
+    public function generate_share($path, $userid) {
+        if (get_config('tool_oauth2sciebo', 'path') === 'http') {
+            $pref = 'http://';
         } else {
-            notice($xml->meta->message, new moodle_url('/mod/collaborativefolders/view.php', array('id' => $id)));
+            $pref = 'https://';
         }
-        return $xml;
+
+        $output = $this->sciebo->get_link($path, $userid);
+
+        $xml = simplexml_load_string($output);
+
+        if ($xml->meta->statuscode == 100 && $xml->meta->status == 'ok') {
+
+            notice(get_string('successtoaddfolder', 'mod_collaborativefolders'),
+                    new moodle_url('/mod/collaborativefolders/view.php'));
+            $fields = explode("/s/", $xml->data[0]->url[0]);
+            $fileid = $fields[1];
+
+            return $pref . get_config('tool_oauth2sciebo', 'server').'/public.php?service=files&t=' . $fileid;
+
+        } else {
+            notice($xml->meta->message,
+                    new moodle_url('/mod/collaborativefolders/view.php'));
+
+            return false;
+        }
     }
 
     /**
@@ -128,51 +124,5 @@ class owncloud_access {
         } else {
             return false;
         }
-    }
-
-    public function get_link($url) {
-        // Hardcoded user data here. Has to be replaced as soon as OAuth is ready.
-        // TODO How can requests be send without user data in clear text?
-//        $sciebo = new \tool_oauth2sciebo\sciebo($returnurl);
-
-//        $sciebo->
-//        $username = 'collaborativefolder.pbox@uni-muenster.de';
-//        $password = '';
-//        $pref = 'https://';
-//
-//        $ch = curl_init();
-//
-//        // A POST request creating a share for the chosen file is generated here.
-//        curl_setopt($ch, CURLOPT_URL, $pref . 'uni-muenster.sciebo.de' . '/ocs/v1.php/apps/files_sharing/api/v1/shares');
-//        curl_setopt($ch, CURLOPT_POST, 1);
-//
-//        // http_build_query additionally needs a new arg_separator ("&" instead of "&amp;")
-//        // to be able to create the message body.
-//        // Additional POST arguments can be edited.
-//        curl_setopt($ch, CURLOPT_POSTFIELDS,
-//            http_build_query(array('path' => $url,
-//                'shareType' => 3,
-//                'publicUpload' => true,
-//                'permissions' => 31,
-//            ), null, "&"));
-//
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//        curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-//        $output = curl_exec($ch);
-//
-//        // The output has to be transformed into an xml file to be able to extract specific arguments
-//        // of the response from the owncloud Server.
-//        $xml = simplexml_load_string($output);
-//
-//        curl_close($ch);
-//
-//        // The unique fileID is extracted from the given shared link.
-//        $fields = explode("/s/", $xml->data[0]->url[0]);
-//        $fileid = $fields[1];
-//
-//        // And then its inserted into a dynamic link that will be provided to the user.
-//        // WARNING: if you wish to generate a link for a local instance of owncloud, the path has to be edited
-//        // in the namespace of the concerning window (e.g. http://localhost/owncloud/...).
-//        return $pref . 'uni-muenster.sciebo.de' . '/public.php?service=files&t=' . $fileid;
     }
 }
