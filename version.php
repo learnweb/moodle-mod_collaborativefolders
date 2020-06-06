396d9f8325ef5515d1bc6a231a4f5fd785d3a200
Justus Dieckmann <justusdieckmann@wwu.de>
2020-06-06 19:06:10 +0200

    New version v3.9-r1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborativefolders';
-$plugin->version = 2019112100;
-$plugin->release = 'v3.8-r1';
+$plugin->version = 2020060600;
+$plugin->release = 'v3.9-r1';
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_STABLE;
